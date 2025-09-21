4b7e701531cedfdcad33e94b50fa32df361e13a8
DenzerNk <4488sveta@gmail.com>
2025-09-21 20:30:39 +0300

    Add files via upload

---
+++ CnApp_Python_Lab_2_quest_1.py
@@ -0,0 +1,19 @@
+﻿import math
+
+def expression(alpha):
+    z = (math.sqrt(2) / 2) * math.sin(alpha / 2)
+    return z
+
+def amoeba(n):
+    k = n // 3
+    return 2 ** k
+
+alpha = float(input("Enter a =: "))
+print("Result z = ", expression(alpha))
+
+n = int(input("Enter the number hours n: "))
+
+while n < 0:
+    n = int(input("Number hours negative, enter n again: "))
+
+print("Result amoeba", n, "hours =", amoeba(n))
