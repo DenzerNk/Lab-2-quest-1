ceba70eacd8a29bcbcb9169e7205693532fdc10d
DenzerNk <4488sveta@gmail.com>
2025-09-23 00:55:54 +0300

    CnApp_Python_Lab_2_quest_1.py

--- CnApp_Python_Lab_2_quest_1.py
+++ CnApp_Python_Lab_2_quest_1.py
@@ -1,19 +1,12 @@
-﻿import math
+import math
+from mod import amoeba
 
 def expression(alpha):
-    z = (math.sqrt(2) / 2) * math.sin(alpha / 2)
+    z = (math.sqrt(2) / 2) * math.sin(alpha / 2) - alpha / 2
     return z
 
-def amoeba(n):
-    k = n // 3
-    return 2 ** k
+alpha = float(input("Введіть значення α (в радіанах): "))
+print("Значення виразу z = ", expression(alpha))
 
-alpha = float(input("Enter a =: "))
-print("Result z = ", expression(alpha))
-
-n = int(input("Enter the number hours n: "))
-
-while n < 0:
-    n = int(input("Number hours negative, enter n again: "))
-
-print("Result amoeba", n, "hours =", amoeba(n))
+n = int(input("Введіть кількість годин n: "))
+print("Кількість амеб через", n, "годин = ", amoeba(n))
